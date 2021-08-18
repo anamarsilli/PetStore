720738c0fa9b7999f0d30a9e96158f2237b4e865
Ana Marsilli <ana.marsilli@gmail.com>
2021-08-18 20:54:40 -0300

    Incluído teste de consulta de Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,14 +11,16 @@ import static org.hamcrest.Matchers.is;
 
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
+    Long petId;
 
     public String readData(String urlData) throws IOException {
         return new String(Files.readAllBytes(Paths.get(urlData)));
     }
 
-    @Test // Anotation que identifica o método ou função como um teste para o TestNG
+    @Test (priority = 0)// Anotation que identifica o método ou função como um teste para o TestNG
     public void createPet() throws IOException {
         String jsonBody = readData("data/pet1.json");
+        petId =
 
         // Sintaxe Gherkin - Dado | Quando | Então - Given | When | Then
         given()
@@ -34,6 +36,24 @@ public class Pet {
                 .body("status", is("available"))
                 .body("category.name", is("dog"))
                 .body("tags.name", contains("lovely", "iterasys"))
+        .extract()
+                .path("id")
+        ;
+        System.out.println("O id do pet é " + petId);
+    }
+
+    @Test (priority = 1)
+    public void getPet(){
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId.toString())
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Snoopy"))
         ;
     }
 }
